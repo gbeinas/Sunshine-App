4bcc3b527b168255ce84ecd7c4c33a833bfe4233
George Beinas <gbeinas@di.uoa.gr>
2016-01-04 13:14:53 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/Forecasts.java
@@ -0,0 +1,22 @@
+package com.example.android.sunshine;
+
+/**
+ * Created by George Beinas on 4/1/2016.
+ */
+public class Forecasts {
+
+    String [] forecast = {
+            "Today Sunny - 83/56",
+            "Sunday Sunny - 83/56",
+            "Monday Sunny - 83/56",
+            "Thuesday Sunny - 83/56",
+            "Thursday Sunny - 83/56",
+            "Friday Sunny - 83/56",
+            "Saturday Sunny - 83/56",
+            "Friday Sunny - 83/56",
+            "Friday Sunny - 83/56",
+            "Friday Sunny - 83/56",
+            "Friday Sunny - 83/56",
+            "Friday Sunny - 83/56",
+            "Friday Sunny - 83/56",};
+}
